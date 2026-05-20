7f09cb24b1b590e698d3a07978a003a7bfc95016
SantiagoMachadoCastillo <smachadoc@libertadores.edu.co>
2026-05-20 12:05:50 -0500

    Spotlight

--- main.js
+++ main.js
@@ -14,8 +14,16 @@ const material = new THREE.MeshPhysicalMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
+// white spotlight shining from the side, modulated by a texture
+const spotLight = new THREE.SpotLight( 0xffffff );
+spotLight.position.set( 100, 1000, 100 );
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.near = 500;
+spotLight.shadow.camera.far = 4000;
+spotLight.shadow.camera.fov = 30;
+scene.add( spotLight );
 
 camera.position.z = 5;
 
